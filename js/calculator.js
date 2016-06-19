133ff708bf2f2b9cbdd917bbb3793df303a9fbdf
Alberto Iglesias Gallego <alberto.uchiha@gmail.com>
2016-06-19 20:24:37 +0200

    add functions to recognize just numbers and operators

--- js/calculator.js
+++ js/calculator.js
@@ -2,16 +2,50 @@
  * Calculator functionality library
  * @type {Function}
  */
-var Calculator = (function(id) {
+var Calculator = (function(idInputCalc, idCalcButton) {
 
-	var idInput = id;
+	var idInput = idInputCalc;
+	var idCalcButton = idCalcButton;
 	var oMathLibrary = new mathLibrary();
 
 	var checkText = function(e) {
-		if(_checkIfCharacterIsANumber(e) ||
-			_checkIfCharacterIsAnOperator(e)){
 
+		_pressCalcButtonIfIsPressedOnInput(e);
+
+		return !!(_checkIfCharacterIsANumber(e) ||
+		_checkIfCharacterIsAnOperator(e));
+	};
+
+	var _pressCalcButtonIfIsPressedOnInput = function(e) {
+		if(e.keyCode === 13){
+			return document.getElementById(idCalcButton).click();
 		}
+	};
+
+	/**
+	 * Check if character is a number
+	 * @private
+	 * @param e
+	 * @returns {boolean}
+	 */
+	var _checkIfCharacterIsANumber = function(e) {
+		var key = window.Event ? e.which : e.keyCode;
+		return (key >= 48 && key <= 57)
+	};
+
+	/**
+	 * Check if a character is an operator
+	 * @private
+	 * @param e
+	 * @returns {boolean}
+	 */
+	var _checkIfCharacterIsAnOperator = function(e) {
+		var key = window.Event ? e.which : e.keyCode;
+		return (key === 191 ||
+				key === 42 ||
+				key === 45 ||
+				key === 94 ||
+				key === 43)
 	};
 
 	/**
@@ -25,7 +59,9 @@ var Calculator = (function(id) {
 
 	/**
 	 * Clean selected input text
-	 */
+	 * @param numberOfCharacters
+	 * @returns {*}
+     */
 	var cleanInput = function(numberOfCharacters) {
 		if(numberOfCharacters === 0) {
 			return document.getElementById(idInput).value = '';
@@ -42,28 +78,6 @@ var Calculator = (function(id) {
 		var inputCalculator = document.getElementById(idInput);
 		var length = inputCalculator.value.length - numberOfCharacters;
 		return document.getElementById(idInput).value.substr(0, length);
-	};
-
-	/**
-	 * Check if character is a number
-	 * @private
-	 * @param e
-	 * @returns {boolean}
-     */
-	var _checkIfCharacterIsANumber = function(e) {
-		var key = window.Event ? e.which : e.keyCode;
-		return (key >= 48 && key <= 57)
-	};
-
-	/**
-	 * Check if a character is an operator
-	 * @private
-	 * @param e
-	 * @returns {boolean}
-     */
-	var _checkIfCharacterIsAnOperator = function(e) {
-		var key = window.Event ? e.which : e.keyCode;
-		return (key >= 48 && key <= 57)
 	};
 
 	/**
